d7fb7271b74f352aad3ac497162f5a0a1b4ee3b0
Rashid Jama <rashidjama5@gmail.com>
2019-12-18 03:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var kittens = [] //define your array here
+var kittens = ['Milo','Otis','Garfield'] //define your array here
 
 // Add your functions and code here
+  function destructivelyAppendKitten(name) {
+    kittens.push(name);
+    return kittens;
+  }
+  function destructivelyPrependKitten(name) {
+    kittens.unshift(name);
+    return kittens;
+  }
+  function destructivelyRemoveLastKitten(name) {
+    kittens.pop(name);
+    return kittens;
+  }
+  function destructivelyRemoveFirstKitten(name) {
+    kittens.shift(name);
+    return kittens;
+  }
+  function appendKitten(name) {
+    return kittens.concat(name);
+  }
+  function prependKitten(name) {
+    kittens.splice(0, 0, name);
+    return kittens;
+  }
+  
+  
